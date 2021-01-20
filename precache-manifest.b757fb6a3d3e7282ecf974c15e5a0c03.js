8dc678a51cdb7b42f1d26412e0563b4a3fc3a036
Jake Cawley <jcawl123@gmail.com>
2021-01-20 14:23:56 -0500

    Updates

---
+++ precache-manifest.b757fb6a3d3e7282ecf974c15e5a0c03.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ddb43c1ad2b7159c330cf95a5748343",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7a17b748e094308ba2b5",
+    "url": "./static/css/main.4b352c1f.chunk.css"
+  },
+  {
+    "revision": "4c751d99a56113e98bd1",
+    "url": "./static/js/2.b3c97b00.chunk.js"
+  },
+  {
+    "revision": "1fb20645ae74b3f508b75afe1533c228",
+    "url": "./static/js/2.b3c97b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a17b748e094308ba2b5",
+    "url": "./static/js/main.3c1c4f27.chunk.js"
+  },
+  {
+    "revision": "0d30e5945f64273442ea",
+    "url": "./static/js/runtime-main.b41914ef.js"
+  }
+]);
